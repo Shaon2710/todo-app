8b6e3567a1b0a7572ebd0f8da01dacb388831be9
Shaon2710 <shaonk4@gmail.com>
2018-05-20 15:17:43 +0600

    done

--- js/main.js
+++ js/main.js
@@ -1,61 +1,51 @@
 const taskList = document.querySelector("#task-list");
 const taskList1 = document.querySelector("#task-list1");
 
-
-taskList.addEventListener('click', function(e) {
+taskList.addEventListener('click', (e) => {
 
     if (e.target.classList.contains('done')) {
         let li = e.target.parentElement;
-        let task = li.textContent;
-
         li.parentElement.removeChild(li);
+
+        let x = li.querySelector("span");
+        let task = x.textContent;
 
         let liOne = document.createElement('li');
         let taskName = document.createElement('span');
         let doneBtn = document.createElement('span');
 
-        // assign content
         taskName.textContent = task;
         doneBtn.textContent = "Undo";
 
-        // // Add class
         liOne.classList.add("list-group-item");
         taskName.classList.add("task-name");
         doneBtn.classList.add("done", "btn", "btn-outline-success", "float-right");
 
-        // // append child
         liOne.appendChild(taskName);
         liOne.appendChild(doneBtn);
         taskList1.appendChild(liOne);
-
     }
 });
 
-
-taskList1.addEventListener('click', function(e) {
+taskList1.addEventListener('click', (e) => {
     if (e.target.classList.contains('done')) {
         let li = e.target.parentElement;
         li.parentElement.removeChild(li);
 
-
-        let task = li.textContent;
-
-
+        let x = li.querySelector("span");
+        let task = x.textContent;
 
         let liOne = document.createElement('li');
         let taskName = document.createElement('span');
         let doneBtn = document.createElement('span');
 
-        // assign content
         taskName.textContent = task;
         doneBtn.textContent = "Done";
 
-        // // Add class
         liOne.classList.add("list-group-item");
         taskName.classList.add("task-name");
         doneBtn.classList.add("done", "btn", "btn-outline-success", "float-right");
 
-        // // append child
         liOne.appendChild(taskName);
         liOne.appendChild(doneBtn);
         taskList.appendChild(liOne);
@@ -65,26 +55,22 @@ taskList1.addEventListener('click', function(e) {
 
 const addForm = document.forms['add-task-form'];
 
-addForm.addEventListener('submit', function(e) {
+addForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let task = addForm.querySelector('input[name="task"]').value;
     if (task) {
-        // Create DOM Element
 
         let li = document.createElement('li');
         let taskName = document.createElement('span');
         let doneBtn = document.createElement('span');
 
-        // assign content
         taskName.textContent = task;
         doneBtn.textContent = "done";
 
-        // // Add class
         li.classList.add("list-group-item");
         taskName.classList.add("task-name");
         doneBtn.classList.add("done", "btn", "btn-outline-success", "float-right");
 
-        // // append child
         li.appendChild(taskName);
         li.appendChild(doneBtn);
         taskList.appendChild(li);
@@ -94,5 +80,30 @@ addForm.addEventListener('submit', function(e) {
         alert("Please add your task");
         addForm.querySelector("input").focus();
     }
+});
 
+const searchInput = document.getElementById('serch-input');
+
+searchInput.addEventListener('keyup', (e) => {
+    let s = e.target.value.toLowerCase();
+    let taks = taskList.querySelectorAll("li");
+    let taks1 = taskList1.querySelectorAll("li");
+
+    taks.forEach((t) => {
+        let taskContent = t.querySelector('.task-name').textContent.toLowerCase();
+        if (taskContent.indexOf(s) != -1) {
+            t.style.display = "block";
+        } else {
+            t.style.display = "none";
+        }
+    });
+
+    taks1.forEach((t1) => {
+        let taskContent1 = t1.querySelector('.task-name').textContent.toLowerCase();
+        if (taskContent1.indexOf(s) != -1) {
+            t1.style.display = "block";
+        } else {
+            t1.style.display = "none";
+        }
+    });
 });
